f3a1e7d3e6601aefeb31dbfa30efbc2b9050c045
rmistr00 <rmistr00@g.uafs.edu>
2022-11-01 17:06:18 -0500

    x

--- src/layers/team.js
+++ src/layers/team.js
@@ -1,9 +1,12 @@
 import { motion } from "framer-motion";
 
+import "./team.scss";
+
 function Team({ setLayer }) {
   return (
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} id="Team">
       <button
+        id="home-button"
         onClick={() => {
           setLayer("home");
         }}
