1b333cea690045c93a77463c872c01314335fcf3
rmistr00 <rmistr00@g.uafs.edu>
2022-10-31 22:09:32 -0500

    x

---
+++ src/functions/local-storage.js
@@ -0,0 +1,19 @@
+export const LS = {
+  init() {
+    this.name = "pokedex-alpha";
+    let data = JSON.parse(localStorage.getItem(this.name));
+    if (data !== null) {
+      this.data = data;
+    } else {
+      this.data = {
+        caught: {
+          Bulbasaur: true,
+        },
+      };
+      this.save(this.data);
+    }
+  },
+  save(data) {
+    localStorage.setItem(this.name, JSON.stringify(data));
+  },
+};
